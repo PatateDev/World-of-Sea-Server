64496e658431ef0f01aa81f1bf1b4b785c319636
mathdu07 <mathdu07.minecraft@gmail.com>
2014-03-07 23:33:43 +0100

    Implement MySQL Database

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,23 @@
-#include <iostream>
-#include <cstdlib>
-#include <SFML/Network.hpp>
+#include "Main.h"
+#include "SQL/SQLDatabase.h"
 
 int main(int argc, char **argv)
 {
-	std::cout << "Hello world" << std::endl;
+	std::cout << "Starting World of Sea Server v" << SERVER_VERSION_MAJOR <<
+	        "." << SERVER_VERSION_MINOR << "." << SERVER_VERSION_PATCH << std::endl;
+
+	if (argc != 5)
+	{
+	    std::cout << "The server must be started with the following MySQL arguments : ";
+	    std::cout << "<username> <password> <address> <database>" << std::endl;
+	    return EXIT_FAILURE;
+	}
+
+	SQLDatabase database;
+	if (!database.init(argv[1], argv[2], argv[3], argv[4]))
+	{
+	    return EXIT_FAILURE;
+	}
+
 	return EXIT_SUCCESS;
 }
